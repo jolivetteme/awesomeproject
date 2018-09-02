56023f3d27baacdfcd7a0d598cf07a57838e9743
Mitchell Jolivette <jolivetteme@ubuntu16.04-K55A>
2018-09-02 18:25:36 -0500

    Greeting Tutorial

--- App.js
+++ App.js
@@ -7,45 +7,25 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+// import {Platform, StyleSheet, Text, View, AppRegistry, Image} from 'react-native';
+import { AppRegistry, Text, View } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+class Greeting extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>It has taken me so long to figure this out!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <Text style={styles.welcome}>Thank you for stopping by!</Text>
-      </View>
+      <Text>Hello {this.props.name}!</Text>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 30,
-    textAlign: 'center',
-    margin: 10,
-    fontWeight:'bold'
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
+    );
+  }
+}
